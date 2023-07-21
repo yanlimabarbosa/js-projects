e5bdac94de0603a70db0456c49300a213d0fd138
Yan Lima Barbosa <yanbr763@gmail.com>
2023-07-21 08:23:45 -0300

    pause function and preloader added

---
+++ 09-video/app.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector('.switch-btn')
+const video = document.querySelector('.video-container')
+
+btn.addEventListener('click',()=>{
+  btn.classList.toggle('slide')
+  video.paused ? video.play() : video.pause()
+})
+
+const preloader = document.querySelector(".preloader")
+
+window.addEventListener("load", function () {
+  preloader.classList.add("hide-preloader")
+})
+
+
