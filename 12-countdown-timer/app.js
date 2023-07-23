0b399fa8c3f5f4e4725792de5d11aef312043880
Yan Lima Barbosa <yanbr763@gmail.com>
2023-07-23 12:32:45 -0300

    time remaining calculated and setted dinamycally using new Date()

--- 12-countdown-timer/app.js
+++ 12-countdown-timer/app.js
@@ -26,7 +26,7 @@ const giveaway = document.querySelector(".giveaway")
 const deadline = document.querySelector(".deadline")
 const items = document.querySelectorAll(".deadline-format h4")
 
-let futureDate = new Date(2023, 4, 24, 17, 30, 0)
+let futureDate = new Date(2024, 1, 27, 10, 0, 0)
 
 const year = futureDate.getFullYear()
 
@@ -41,3 +41,34 @@ const weekday = weekdays[futureDate.getDay()]
 const date = futureDate.getDate()
 
 giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`
+
+const futureTime = futureDate.getTime()
+
+function getRemainingTime() {
+  const today = new Date().getTime()
+  const t = futureTime - today
+
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000
+  const oneHour = 60 * 60 * 1000
+  const oneMinute = 60 * 1000
+
+  let days = t / oneDay
+  days = Math.floor(days)
+  let hours = Math.floor((t % oneDay) / oneHour)
+  let minutes = Math.floor((t % oneHour) / oneMinute)
+  let secs = Math.floor((t % oneMinute) / 1000)
+
+  // set values array
+  const values = [days, hours, minutes, secs]
+
+  const format = (item) => {
+    return item < 10 ? `0${item}` : item
+  }
+
+  items.forEach((item, index) => {
+    item.innerHTML = format(values[index])
+  })
+}
+
+getRemainingTime()
