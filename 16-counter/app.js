f6ab6b223f07001ed72abc7e8db63c623fea694e
Yan Lima Barbosa <yanbr763@gmail.com>
2023-07-25 18:21:04 -0300

    class refactor

--- 16-counter/app.js
+++ 16-counter/app.js
@@ -8,36 +8,37 @@ function getElement(selection) {
   )
 }
 
-function Counter(element, value) {
-  this.counter = element
-  this.value = value
-  this.starterValue = value
-  this.resetBtn = element.querySelector(".reset")
-  this.increaseBtn = element.querySelector(".increase")
-  this.decreaseBtn = element.querySelector(".decrease")
-  this.valueDOM = element.querySelector(".value")
-  this.valueDOM.textContent = this.value
+class Counter {
+  constructor(element, value) {
+    this.counter = element
+    this.value = value
+    this.starterValue = value
+    this.resetBtn = element.querySelector(".reset")
+    this.increaseBtn = element.querySelector(".increase")
+    this.decreaseBtn = element.querySelector(".decrease")
+    this.valueDOM = element.querySelector(".value")
+    this.valueDOM.textContent = this.value
 
-  this.decreaseBtn.addEventListener("click", this.decrease.bind(this))
-  this.resetBtn.addEventListener("click", this.reset.bind(this))
-  this.increaseBtn.addEventListener("click", this.increase.bind(this))
+    this.decreaseBtn.addEventListener("click", this.decrease.bind(this))
+    this.resetBtn.addEventListener("click", this.reset.bind(this))
+    this.increaseBtn.addEventListener("click", this.increase.bind(this))
+  }
+  increase() {
+    console.log(this)
+    this.value++
+    this.valueDOM.textContent = this.value
+  }
+  decrease() {
+    console.log(this)
+    this.value--
+    this.valueDOM.textContent = this.value
+  }
+  reset() {
+    console.log(this)
+    this.value = this.starterValue
+    this.valueDOM.textContent = this.value
+  }
 }
 
-Counter.prototype.increase = function () {
-  console.log(this)
-  this.value++
-  this.valueDOM.textContent = this.value
-}
-Counter.prototype.decrease = function () {
-  console.log(this)
-  this.value--
-  this.valueDOM.textContent = this.value
-}
-Counter.prototype.reset = function () {
-  console.log(this)
-  this.value = this.starterValue
-  this.valueDOM.textContent = this.value
-}
-
-const firstCounter = new Counter(getElement(".first-counter"), 100)
-const secondCounter = new Counter(getElement(".second-counter"), 200)
+const firstCounter = new Counter(getElement(".first-counter"), 0)
+const secondCounter = new Counter(getElement(".second-counter"), 100)
