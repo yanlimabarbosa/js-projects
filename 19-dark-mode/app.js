1fd68ac3e8cf7a961615e4691615e1008bccb829
Yan Lima Barbosa <yanbr763@gmail.com>
2023-07-29 10:41:50 -0300

    feat: toggle dark theme, import data and render items, and moment.js lib added

---
+++ 19-dark-mode/app.js
@@ -0,0 +1,23 @@
+const toggleBtn = document.querySelector(".btn")
+const articlesDOM = document.querySelector(".articles")
+
+toggleBtn.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark-theme")
+})
+const articlesData = articles
+  .map((article) => {
+    const { title, date, length, snippet } = article
+    // format date
+    const formatDate = moment(date).format("MMMM Do, YYYY")
+    return ` <article class="post">
+          <h2>${title}</h2>
+          <div class="post-info">
+            <span>${formatDate}</span>
+            <span>${length} min read</span>
+          </div>
+          <p>${snippet}</p>
+        </article>`
+  })
+  .join("")
+
+  articlesDOM.innerHTML = articlesData
