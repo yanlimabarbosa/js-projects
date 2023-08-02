032d3ff6eb7dd9fe5ab7e1d39ac770166d0b758a
Yan Lima Barbosa <yanbr763@gmail.com>
2023-08-02 19:59:13 -0300

    displayButtons.js

---
+++ 27-pagination/displayButtons.js
@@ -0,0 +1,12 @@
+const displayButtons = (container, pages, activeIndex) => {
+  let btns = pages.map((_, pageIndex) => {
+    return `<button class="page-btn ${
+      activeIndex === pageIndex ? "active-btn" : null
+    }" data-index="${pageIndex}">${pageIndex + 1}</btn>`
+  })
+  btns.push(`<button class="next-btn">next</button>`)
+  btns.unshift(`<button class="next-btn">prev</button>`)
+  container.innerHTML = btns.join("")
+}
+
+export default displayButtons
