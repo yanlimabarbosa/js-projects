2a8d8cf9b40bf25ab286526fccce816a8c658740
Yan Lima Barbosa <yanbr763@gmail.com>
2023-08-02 16:55:26 -0300

    feat: dynamic submenu

--- 26-stripe-submenus/app.js
+++ 26-stripe-submenus/app.js
@@ -34,3 +34,51 @@ sidebar.innerHTML = sublinks
     </article>`
   })
   .join("")
+
+linkBtns.forEach((btn) => {
+  btn.addEventListener("mouseover", (e) => {
+    const text = e.currentTarget.textContent
+    const tempBtn = e.currentTarget.getBoundingClientRect()
+    const center = (tempBtn.left + tempBtn.right) / 2
+    const bottom = tempBtn.bottom - 3
+
+    const tempPage = sublinks.find(({ page }) => page === text)
+    if (tempPage) {
+      const { page, links } = tempPage
+      submenu.classList.add("show")
+      submenu.style.left = `${center}px`
+      submenu.style.top = `${bottom}px`
+
+      // Columns
+      let columns = "col-2"
+      if (links.length === 3) {
+        columns = "col-3"
+      }
+      if (links.length > 3) {
+        columns = "col-4"
+      }
+
+      submenu.innerHTML = `<section>
+      <h4>${page}</h4>
+      <div class="submenu-center ${columns}">
+      ${links
+        .map((link) => {
+          return `<a href="index.html">
+        <i class="${link.icon}"></i> ${link.label}
+        </a>`
+        })
+        .join("")}
+      </div>
+      </section>`
+    } //instead of index.html it should be placed ${link.url}
+  })
+})
+
+hero.addEventListener("mouseover", (e) => {
+  submenu.classList.remove("show")
+})
+nav.addEventListener("mouseover", (e) => {
+  if (!e.target.classList.contains("link-btn")) {
+    submenu.classList.remove("show")
+  }
+})
