1090ed71fdf11e6b3dd9f813868f1cc5c1bd5193
Yan Lima Barbosa <yanbr763@gmail.com>
2023-08-02 19:13:51 -0300

    app.js

---
+++ 27-pagination/app.js
@@ -0,0 +1,16 @@
+import fetchFollowers from "./fetchFollowers.js"
+import displayFollowers from "./displayFollowers.js"
+import paginate from "./paginate.js"
+import displayButtons from "./displayButtons.js"
+
+const title = document.querySelector(".section-title h1")
+
+
+
+const init = async () =>{
+  const followers = await fetchFollowers()
+  displayFollowers(followers)
+  title.textContent = `pagination`
+}
+
+window.addEventListener("load", init)
