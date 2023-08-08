89ef8b258200373591e5fa3ac27f4886aafe9eff
Yan Lima Barbosa <yanbr763@gmail.com>
2023-08-08 17:35:05 -0300

    cart feat: decrease amount

--- 29-comfy-store/src/cart/setupCart.js
+++ 29-comfy-store/src/cart/setupCart.js
@@ -66,7 +66,18 @@ function increaseAmount(id) {
   cart = cart.map((cartItem) => {
     if (cartItem.id === id) {
       newAmount = cartItem.amount + 1
-      cartItem = { ...cartItem, amount: cartItem.amount + 1 }
+      cartItem = { ...cartItem, amount: newAmount }
+    }
+    return cartItem
+  })
+  return newAmount
+}
+function decreaseAmount(id) {
+  let newAmount
+  cart = cart.map((cartItem) => {
+    if (cartItem.id === id) {
+      newAmount = cartItem.amount - 1
+      cartItem = { ...cartItem, amount: newAmount }
     }
     return cartItem
   })
@@ -89,7 +100,15 @@ function setupCartFunctionality() {
       parent.nextElementSibling.textContent = newAmount
     }
     // decrease
-    
+    if (parent.classList.contains("cart-item-decrease-btn")) {
+      const newAmount = decreaseAmount(parentID)
+      if (newAmount === 0) {
+        removeItem(id)
+        parent.parentElement.parentElement.remove()
+      } else {
+        parent.previousElementSibling.textContent = newAmount
+      }
+    }
     displayCartItemCount()
     displayCartTotal()
     setStorageItem("cart", cart)
