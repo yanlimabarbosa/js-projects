bb0abaadbd480a49698b329f3c96fb4eb53ad270
Yan Lima Barbosa <yanbr763@gmail.com>
2023-08-08 17:25:51 -0300

    cart feat: remove item

--- 29-comfy-store/src/cart/setupCart.js
+++ 29-comfy-store/src/cart/setupCart.js
@@ -58,7 +58,9 @@ function displayCartItemsDOM() {
     addToCartDOM(cartItem)
   })
 }
-
+function removeItem(id) {
+  cart = cart.filter((cartItem) => cartItem.id !== id)
+}
 function increaseAmount(id) {
   let newAmount
   cart = cart.map((cartItem) => {
@@ -70,7 +72,24 @@ function increaseAmount(id) {
   })
   return newAmount
 }
-function setupCartFunctionality() {}
+function setupCartFunctionality() {
+  cartItemsDOM.addEventListener("click", function (e) {
+    const element = e.target
+    const parent = e.target.parentElement
+    const id = e.target.dataset.id
+    const parentID = e.target.parentElement.dataset.id
+    // remove
+    if (element.classList.contains("cart-item-remove-btn")) {
+      removeItem(id)
+      element.parentElement.parentElement.remove()
+    }
+    // increase
+    // decrease
+    displayCartItemCount()
+    displayCartTotal()
+    setStorageItem("cart", cart)
+  })
+}
 
 const init = () => {
   // display amount of cart items
