cbf0c2d95fd148be7791a340fcb748bff5f081cf
Yan Lima Barbosa <yanbr763@gmail.com>
2023-08-08 16:05:54 -0300

    setupCart.js

--- 29-comfy-store/src/cart/setupCart.js
+++ 29-comfy-store/src/cart/setupCart.js
@@ -10,7 +10,29 @@ import { findProduct } from "../store.js"
 import addToCartDOM from "./addToCartDOM.js"
 // set items
 
+const cartItemDOM = getElement(".cart-item-count")
+const cartItemsDOM = getElement(".cart-items")
+const cartTotalDOM = getElement(".cart-total")
+
+let cart = getStorageItem("cart")
+
 export const addToCart = (id) => {
+  let item = cart.find((cartItem) => cartItem.id === id)
+  if (!item) {
+    let product = findProduct(id)
+    // add item to the cart
+    product = { ...product, amount: 1 }
+    cart = [...cart, product]
+    // add item to the DOM
+    addToCartDOM(product)
+  } else {
+  }
   console.log(id)
+  // more stuff coming up
   openCart()
 }
+
+const init = () => {
+  console.log(cart)
+}
+init()
