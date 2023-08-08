bdb0a6f1a154a2033716df09cae8f03bf4bec28f
Yan Lima Barbosa <yanbr763@gmail.com>
2023-08-08 15:38:56 -0300

    util.js

--- 29-comfy-store/src/utils.js
+++ 29-comfy-store/src/utils.js
@@ -1,6 +1,6 @@
 const allProductsUrl = "https://course-api.com/javascript-store-products"
 const singleProductUrl =
-  "https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog"
+  "https://course-api.com/javascript-store-single-product"
 
 const getElement = (selection) => {
   const element = document.querySelector(selection)
